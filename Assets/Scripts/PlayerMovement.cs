53abb2b96fbe203f71127cbe201836d8c45d4d18
Matthew Wong <matthewwong3.14@gmail.com>
2016-09-29 14:48:48 -0700

    Initial unity setup. Wall prefab, with different materials on the sides, is created. Player movement script initialized as well.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float horSpeed = 0.1f;
+    public float verSpeed = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    void FixedUpdate() {
+        transform.Translate(Input.GetAxis("Horizontal") * horSpeed, 0, Input.GetAxis("Vertical") * verSpeed);
+    }
+}
