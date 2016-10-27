35c4949b698d31426f3c2018102187b3a3fdd85e
Matthew Wong <matthewwong3.14@gmail.com>
2016-10-26 21:46:19 -0700

    Added an option to specify day(bright ambient) or night(low ambient lighting) using a vertex and fragment shader.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    private float night = 0.5f;
+    private float day = 1.0f;
+    private Renderer[] _renderers;
+
+	// Use this for initialization
+	void Start () {
+        int setDay = PlayerPrefs.GetInt("lighting");
+        Debug.Log("lighting set to:" + setDay);
+        float chosenTime = 0;
+
+        if(setDay == 0)
+        {
+            chosenTime = day;
+        } else if(setDay == 1)
+        {
+            chosenTime = night;
+        }
+
+        _renderers = GetComponentsInChildren<Renderer>();
+
+        foreach(Renderer renderer in _renderers)
+        {
+            renderer.material.SetFloat("_AmbientLighIntensity", chosenTime);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour {
+
+    private GameObject dropdownobj;
+    private Dropdown dropdown;
+
+	// Use this for initialization
+	void Start () {
+        dropdownobj = GameObject.Find("DayDropdown");
+        dropdown = dropdownobj.GetComponent<Dropdown>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void goToScene(string scene) {
+        PlayerPrefs.SetInt("lighting", dropdown.value);
+        SceneManager.LoadScene(scene);
+    }
+}
