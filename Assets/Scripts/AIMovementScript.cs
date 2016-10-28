d1aa1eaa114ac812d68b6da056d67ff8e109adb0
Matthew Wong <matthewwong3.14@gmail.com>
2016-10-28 15:09:01 -0700

    Simple AI script implemented with animated model. AI looks towards the position of the player and walks forward.

---
+++ Assets/Scripts/AIMovementScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIMovementScript : MonoBehaviour {
+
+    GameObject playerobj;
+    public float speed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+        playerobj = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 targetPosition = playerobj.transform.position;
+        targetPosition.y = this.transform.position.y;
+        this.transform.LookAt(targetPosition);
+	}
+
+    void FixedUpdate() {
+        moveForward();
+    }
+
+    void moveForward() {
+        transform.position += transform.forward * Time.deltaTime * speed;
+    }
+}
