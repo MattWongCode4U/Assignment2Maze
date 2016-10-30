d7ab577f225987ea4d9021dedf8cc16f5511351d
Spencer Pollock <srepollock@gmail.com>
2016-10-30 16:33:53 -0700

    Player movement working
    Bug of flying when looking up in the air and forward

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,20 +3,72 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
-    public float horSpeed = 0.1f;
-    public float verSpeed = 0.1f;
+	public float speed = 1f;
+
+	private Rigidbody rb;
+
+	public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+	public RotationAxes axes = RotationAxes.MouseXAndY;
+	public float sensitivityX = 15F;
+	public float sensitivityY = 15F;
+	public float minimumX = -360F;
+	public float maximumX = 360F;
+	public float minimumY = -60F;
+	public float maximumY = 60F;
+	float rotationX = 0F;
+	float rotationY = 0F;
+	Quaternion originalRotation;
 
 	// Use this for initialization
 	void Start () {
-	
+		rb = GetComponent<Rigidbody> ();
+		Screen.lockCursor = true;
+
+		// Make the rigid body not change rotation
+		if (rb)
+			rb.freezeRotation = true;
+		originalRotation = transform.localRotation;
+	}
+	public static float ClampAngle (float angle, float min, float max)
+	{
+		if (angle < -360F)
+			angle += 360F;
+		if (angle > 360F)
+			angle -= 360F;
+		return Mathf.Clamp (angle, min, max);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        
+		Vector3 forward = Camera.main.transform.forward * (Input.GetAxis("Vertical") * speed);
+		forward.y = 0f;
+		transform.position += forward;
+		Vector3 right = Camera.main.transform.right * (Input.GetAxis("Horizontal") * speed);
+		right.y = 0f;
+		transform.position += right;
+		if (axes == RotationAxes.MouseXAndY)
+		{
+			rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+			rotationX = ClampAngle (rotationX, minimumX, maximumX);
+			rotationY = ClampAngle (rotationY, minimumY, maximumY);
+			Quaternion xQuaternion = Quaternion.AngleAxis (rotationX, Vector3.up);
+			Quaternion yQuaternion = Quaternion.AngleAxis (rotationY, -Vector3.right);
+			transform.localRotation = originalRotation * xQuaternion * yQuaternion;
+		}
+		else if (axes == RotationAxes.MouseX)
+		{
+			rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+			rotationX = ClampAngle (rotationX, minimumX, maximumX);
+			Quaternion xQuaternion = Quaternion.AngleAxis (rotationX, Vector3.up);
+			transform.localRotation = originalRotation * xQuaternion;
+		}
+		else
+		{
+			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+			rotationY = ClampAngle (rotationY, minimumY, maximumY);
+			Quaternion yQuaternion = Quaternion.AngleAxis (-rotationY, Vector3.right);
+			transform.localRotation = originalRotation * yQuaternion;
+		}
 	}
-
-    void FixedUpdate() {
-        transform.Translate(Input.GetAxis("Horizontal") * horSpeed, 0, Input.GetAxis("Vertical") * verSpeed);
-    }
 }
