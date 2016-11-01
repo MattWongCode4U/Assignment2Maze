f3c050db48ec65ba6d38d7025ecbb9a002965694
Spencer Pollock <srepollock@gmail.com>
2016-10-31 20:51:47 -0700

    Base maze generation complete

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,9 @@
 using System.Collections;
 
 public class GameManager : MonoBehaviour {
+    // Maze
+    public Maze mazePrefab;
+    private Maze mazeInstance;
 
     private float night = 0.5f;
     private float day = 1.0f;
@@ -9,28 +12,41 @@ public class GameManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        int setDay = PlayerPrefs.GetInt("lighting");
-        Debug.Log("lighting set to:" + setDay);
-        float chosenTime = 0;
-
-        if(setDay == 0)
-        {
-            chosenTime = day;
-        } else if(setDay == 1)
-        {
-            chosenTime = night;
-        }
-
-        _renderers = GetComponentsInChildren<Renderer>();
-
-        foreach(Renderer renderer in _renderers)
-        {
-            renderer.material.SetFloat("_AmbientLighIntensity", chosenTime);
-        }
+        BeginGame();
+        SetLighting(PlayerPrefs.GetInt("lighting"));
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (    Input.GetKeyDown(KeyCode.Home) 
+            ||  Input.GetKeyDown(KeyCode.JoystickButton7)) {
+            RestartGame();
+        }
 	}
+
+    void BeginGame() {
+        mazeInstance = Instantiate(mazePrefab) as Maze;
+        mazeInstance.transform.parent = this.transform;
+        StartCoroutine(mazeInstance.Generate());
+    }
+
+    void SetLighting(int lighting) {
+        // Debug.Log("lighting set to:" + lighting);
+        float chosenTime = 0;
+        if(lighting == 0) {
+            chosenTime = day;
+        } else if(lighting == 1) {
+            chosenTime = night;
+        }
+        _renderers = GetComponentsInChildren<Renderer>();
+        foreach(Renderer renderer in _renderers) {
+            renderer.material.SetFloat("_AmbientLighIntensity", chosenTime);
+        }
+    }
+
+    void RestartGame() {
+        StopAllCoroutines();
+        Destroy(mazeInstance.gameObject);
+		BeginGame();
+    }
 }
