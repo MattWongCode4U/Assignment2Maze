b69791b700fc87e203dd62649d5cd6288e624cd5
Matthew Wong <matthewwong3.14@gmail.com>
2016-11-24 19:57:12 -0800

    Master (#23)
    * idk 2
    
    * Version 0.1.2! (#20)
    
    * Left some conflicts in the game
    
    * Update README.md
    
    * :panda_face: Fixed shading, fixed AI height? Work on UI noclip toggle, toggle for fog UI, respawn looks forward
    
    * :beer: Mobile UI added and working.
    
    * :beer: Created an array of walls and the program chooses the correct wall prefab based on the direction
    
    * :fire: Rotation fixed
    
    * :worried: Three finger tap respawn instead of two finger tap
    
    * Start of assignment 3. Added music to the ai that falls off if he's too far away. Press 'P' to toggle stop/play the music. Turning the fog on will have the music at 50%. Day time and night time have different music playing.
    
    * Added saving, loading and deleting the game state. Game state is the score, player's position and ai's position.

---
+++ Assets/Scripts/PlayerScoreUI.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerScoreUI : MonoBehaviour {
+    Text txt;
+
+	// Use this for initialization
+	void Start () {
+        txt = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        txt.text = "Player Score: " + PlayerPrefs.GetInt("playerscore");
+	}
+}
